d36882cab2222f222969bf178fba1843d1036ac1
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2022-07-06 15:17:14 +0200

    refs #initial first commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# CiviCRM extension: be.ctrl.benormalised
+
+Everything functionally related to normalisation of contact data
+- [Usage](#usage)
+
+## Usage
+
+(* FIXME *)
+
+## Getting Started
+
+(* FIXME *)
+
+## Known Issues
+
+(* FIXME *)
---
+++ benormalised.php
@@ -0,0 +1,144 @@
+<?php
+
+require_once 'benormalised.civix.php';
+// phpcs:disable
+use CRM_Benormalised_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function benormalised_civicrm_config(&$config) {
+  _benormalised_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
+ */
+function benormalised_civicrm_xmlMenu(&$files) {
+  _benormalised_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function benormalised_civicrm_install() {
+  _benormalised_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function benormalised_civicrm_postInstall() {
+  _benormalised_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function benormalised_civicrm_uninstall() {
+  _benormalised_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function benormalised_civicrm_enable() {
+  _benormalised_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function benormalised_civicrm_disable() {
+  _benormalised_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function benormalised_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _benormalised_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
+ */
+function benormalised_civicrm_managed(&$entities) {
+  _benormalised_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
+ */
+function benormalised_civicrm_caseTypes(&$caseTypes) {
+  _benormalised_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
+ */
+function benormalised_civicrm_angularModules(&$angularModules) {
+  _benormalised_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
+ */
+function benormalised_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _benormalised_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function benormalised_civicrm_entityTypes(&$entityTypes) {
+  _benormalised_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_themes().
+ */
+function benormalised_civicrm_themes(&$themes) {
+  _benormalised_civix_civicrm_themes($themes);
+}
