6d2b033ddab06f45ea2debee8d18a5d5fb5a1656
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2022-07-07 14:31:45 +0200

    refs #contactfield added function and limit parameters.

--- api/v3/Benormalised/Contactfield.php
+++ api/v3/Benormalised/Contactfield.php
@@ -1,5 +1,6 @@
 <?php
 
+use Civi\Api4\Contact;
 use CRM_ctrl_Benormalised_ExtensionUtil as E;
 
 /**
@@ -13,6 +14,8 @@ use CRM_ctrl_Benormalised_ExtensionUtil as E;
 function _civicrm_api3_benormalised_Contactfield_spec(&$spec) {
   $spec['Field']['api.required'] = 1;
   $spec['Plugin']['api.required'] = 1;
+  $spec['Function']['api.required'] = 1;
+  $spec['Limit']['api.required'] = 1;
 }
 
 /**
@@ -29,9 +32,12 @@ function _civicrm_api3_benormalised_Contactfield_spec(&$spec) {
  */
 function civicrm_api3_benormalised_Contactfield($params) {
 
-  if (array_key_exists('Field', $params) && array_key_exists('Plugin', $params)) {
+  if (array_key_exists('Field', $params)
+    && array_key_exists('Plugin', $params)
+    && array_key_exists('Function', $params)
+    && array_key_exists('Limit', $params)) {
 
-    $contactFields = \Civi\Api4\Contact::get()
+    $contactFields = Contact::get()
       ->addSelect($params['Field'])
       ->addWhere($params['Field'], 'IS NOT EMPTY')
       ->execute();
@@ -39,13 +45,27 @@ function civicrm_api3_benormalised_Contactfield($params) {
     $normalise = [];
     foreach ($contactFields as $contactField) {
       if (isset($contactField[$params['Field']])) {
-        $plugin = new $params['Plugin'];
-        $A = $contactField[$params['Field']];
-        $B = $plugin->convert($A);
-        // @todo remove logging.
-        if($A != $B) {
-          $normalise[] = $contactField;
-          \Drupal::logger('normalise:')->notice($A . "==" . $B);
+        if (class_exists($params['Plugin'])) {
+          $plugin = new $params['Plugin'];
+          $function = (string) $params['Function'];
+          if (method_exists($plugin, $function)) {
+            $A = $contactField[$params['Field']];
+            $B = $plugin->$function($A);
+            if ($A != $B) {
+              $item["id"] = $contactField["id"];
+              $item["A"] = $A;
+              $item["B"] = $B;
+              $normalise[] = $item;
+              // @todo remove logging.
+              Drupal::logger('benormalised')->notice(print_r($item, TRUE));
+            }
+          }
+          else {
+            throw new API_Exception('Incorrect function');
+          }
+        }
+        else {
+          throw new API_Exception('Incorrect plugin');
         }
       }
     }
@@ -53,13 +73,14 @@ function civicrm_api3_benormalised_Contactfield($params) {
     $returnValues = [
       'Field' => $params['Field'],
       'Plugin' => $params['Plugin'],
+      'Function' => $params['Function'],
+      'Limit' => $params['Limit'],
       'values' => count($normalise) . "/" . $contactFields->count(),
     ];
     // Spec: civicrm_api3_create_success($values = 1, $params = [], $entity = NULL, $action = NULL)
     return civicrm_api3_create_success($returnValues, $params, 'Benormalised', 'Customfield');
-
   }
   else {
-    throw new API_Exception(/*error_message*/ 'Incorrect required parameters', /*error_code*/ 'params_incorrect');
+    throw new API_Exception('Incorrect required parameters');
   }
 }
