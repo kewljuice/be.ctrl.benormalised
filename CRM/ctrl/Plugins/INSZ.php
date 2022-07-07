362f59993606b9cf4bbba79abc3c2dec6b0d1cd5
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2022-07-07 14:38:11 +0200

    refs #plugins updated INSC plugin.

--- CRM/ctrl/Plugins/INSZ.php
+++ CRM/ctrl/Plugins/INSZ.php
@@ -2,8 +2,30 @@
 
 class CRM_ctrl_Plugins_INSZ {
 
-  public function convert($insz) {
+
+  /**
+   * @param $insz
+   *
+   * @return string
+   */
+  public function clean($insz) {
     return preg_replace("/\D/", "", $insz);
   }
 
+  /**
+   * @param $insz
+   *
+   * @return string
+   */
+  public function format($insz) {
+    if ($insz === NULL || strlen($insz) <> 11) {
+      return $insz;
+    }
+    $year = substr($insz, 0, 2);
+    $month = substr($insz, 2, 2);
+    $day = substr($insz, 4, 2);
+    $counter = substr($insz, 6, 3);
+    $check = substr($insz, 9, 2);
+    return sprintf('%s.%s.%s-%s.%s', $year, $month, $day, $counter, $check);
+  }
 }
